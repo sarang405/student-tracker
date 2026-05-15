c08d6df8dd76187f6f8d69046d8be75405937d34
sarang405 <sarngdas57@gmail.com>
2026-05-15 15:12:39 +0530

    1st

---
+++ backend/database.py
@@ -0,0 +1,36 @@
+import aiosqlite
+import os
+
+DB_PATH = os.path.join(os.path.dirname(__file__),"tracker.db")
+
+
+async def get_db():
+    async with aiosqlite.connect(DB_PATH) as db:
+        db.row_factory = aiosqlite.Row
+        yield db
+
+async def init_db():
+    async with aiosqlite.connect(DB_PATH) as db:
+        await db.execute("""
+            CREATE TABLE IF NOT EXISTS users(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT UNIQUE NOT NULL,
+                email TEXT UNIQUE NOT NULL,
+                hashed_password TEXT NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                         )
+         """)
+        await db.execute("""
+            CREATE TABLE IF NOT EXISTS activities(
+                id INTEGER PRIMARY AUTOINCREMENT,
+                student_id TEXT NOT NULL,
+                student_name TEXT NOT NULL,
+                activity TEXT NOT NULL,
+                hours REAL NOT NULL,
+                date TEXT NOT NULL,
+                user_id INTEGER NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (user_id) REFERENCES users(id)                          
+                         )
+        """)
+        await db.commit()
